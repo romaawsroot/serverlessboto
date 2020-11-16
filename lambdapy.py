187984af697c0f88b9756bcb34c249e01bd0c579
romaawsroot <73908038+romaawsroot@users.noreply.github.com>
2020-11-17 00:11:05 +0400

    Update lambdapy.py

--- lambdapy.py
+++ lambdapy.py
@@ -2,7 +2,7 @@ import boto3
 def handler(event, context):
     client = boto3.client('s3')
     response = client.create_bucket(
-    Bucket='examplebucketromaawsrootyyyy454545t33t77997799',
+    Bucket='examplebucketromaawsro5t33t7799779988',
     CreateBucketConfiguration={
         'LocationConstraint': 'us-west-2',},
     )
